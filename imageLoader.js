d5526bb2950a614e59d67c3ee848bf396cd82013
ThermiteFe8 <86014765+ThermiteFe8@users.noreply.github.com>
2023-02-20 13:52:06 -0500

    more image load test also renamed some files

--- imageLoader.js
+++ imageLoader.js
@@ -2,6 +2,9 @@
 function loadImages(dirName, imageExtension) {
     var penisDirectory = "/Thermite Portfolio/" + dirName;
     document.getElementById('listHolder').src = penisDirectory + "/fileNames.txt";
-
+    var allFiles = document.getElementById('listHolder').innerHTML;
+    var fileArray = allFiles.split("\n");
+    console.log(fileArray);
+    fileArray[0] = document.getElementById('listOfFiles').innerHTML
 
 }
