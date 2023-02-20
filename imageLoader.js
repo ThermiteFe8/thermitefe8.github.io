be9301a9226d6ec0665b7087a82fc796411123e8
ThermiteFe8 <86014765+ThermiteFe8@users.noreply.github.com>
2023-02-20 15:07:45 -0500

    super duper image loading test

--- imageLoader.js
+++ imageLoader.js
@@ -10,10 +10,35 @@ function loadImages(dirName, imageExtension) {
     req.open("GET", penisDirectory);
     req.send();
     console.log(req);
-
-    document.getElementById('listHolder').src = penisDirectory;
     var allFiles = req.split("\n");
     console.log(allFiles[0]);
+
+    var mediaFiles = [];
+
+    for (var i = 0; i < allFiles.length; i++) {
+        if (allFiles[i].includes(imageExtension)) {
+            mediaFiles.push(allFiles[i]);
+        }
+    }
+
+
+    mediaFiles.sort(function (a, b) {
+        var firstArray = a.split(" - ");
+        var secondArray = b.spit(" - ");
+
+        return firstArray[0] - secondArray[0];
+    });
+
+
+    console.log(mediaFiles);
+    var ballsDirectory = "/Thermite Portfolio" + dirName + "/";
+
+    for (var i = 0; i < mediaFiles.length; i++) {
+        var arraySections = mediaFIles[i].split(" - ");
+        var funnyImage = document.createElement(img);
+        funnyImage.src = ballsDirectory + mediaFiles[i];
+        document.getElementById('imageHolder').appendChild(funnyImage);
+    }
    
 
 }
