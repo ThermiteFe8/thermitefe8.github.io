c232e9284c3c38cbff17a7d32fbaf164f8ab5963
ThermiteFe8 <86014765+ThermiteFe8@users.noreply.github.com>
2023-02-20 13:27:59 -0500

    Rework filesystem and image load test

---
+++ imageLoader.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+const fs = require('fs');
+function loadImages(dirName, imageExtension) {
+    var penisDirectory = "/Thermite Portfolio/" + dirName;
+    document.getElementById('listOfFiles').src = penisDirectory + "/fileNames.txt";
+
+
+}
