fc344a018f93c0d6274e143e069c163d62099f54
ThermiteFe8 <86014765+ThermiteFe8@users.noreply.github.com>
2023-02-20 14:14:33 -0500

    more text file shenanigans

--- imageLoader.js
+++ imageLoader.js
@@ -1,10 +1,22 @@
 // JavaScript source code
 function loadImages(dirName, imageExtension) {
-    var penisDirectory = "/Thermite Portfolio/" + dirName;
-    document.getElementById('listHolder').src = penisDirectory + "/fileNames.txt";
+    var penisDirectory = "/Thermite Portfolio/" + dirName + "/fileNames.txt";
+    document.getElementById('listHolder').src = penisDirectory;
     var allFiles = document.getElementById('listHolder').innerHTML;
     var fileArray = allFiles.split("\n");
     console.log(fileArray);
-    fileArray[0] = document.getElementById('listOfFiles').innerHTML
+    fileArray[0] = document.getElementById('listOfFiles').innerHTML;
+    var txtFile = new XMLHttpRequest();
+    txtFile.open("GET", "penisDirectory", true);
+    txtFile.onreadystatechange = function () {
+        if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
+            if (txtFile.status === 200) {  // Makes sure it's found the file.
+                allText = txtFile.responseText;
+                lines = txtFile.responseText.split("\n"); // Will separate each line into an array
+            }
+        }
+    }
+    console.log(lines[0]);
+    txtFile.send(null);
 
 }
