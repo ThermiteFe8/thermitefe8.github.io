83032e4435ac9aaf83b596c20370dff00044112d
ThermiteFe8 <86014765+ThermiteFe8@users.noreply.github.com>
2023-02-20 14:40:44 -0500

    more text file loading shenanigans

--- imageLoader.js
+++ imageLoader.js
@@ -1,22 +1,19 @@
 // JavaScript source code
 function loadImages(dirName, imageExtension) {
     var penisDirectory = "/Thermite Portfolio/" + dirName + "/fileNames.txt";
-    document.getElementById('listHolder').src = penisDirectory;
-    var allFiles = document.getElementById('listHolder').innerHTML;
-    var fileArray = allFiles.split("\n");
-    console.log(fileArray);
-    fileArray[0] = document.getElementById('listOfFiles').innerHTML;
-    var txtFile = new XMLHttpRequest();
-    txtFile.open("GET", "penisDirectory", true);
-    txtFile.onreadystatechange = function () {
-        if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
-            if (txtFile.status === 200) {  // Makes sure it's found the file.
-                allText = txtFile.responseText;
-                lines = txtFile.responseText.split("\n"); // Will separate each line into an array
-            }
-        }
+    function reqListener() {
+        console.log(this.responseText);
     }
-    console.log(lines[0]);
-    txtFile.send(null);
+    var penisDirectory = "/Thermite Portfolio/" + dirName + "/fileNames.txt";
+    const req = new XMLHttpRequest();
+    req.addEventListener("load", reqListener);
+    req.open("GET", penisDirectory);
+    req.send();
+    console.log(req);
+
+    document.getElementById('listHolder').src = penisDirectory;
+    var allFiles = req.split("\n");
+    console.log(allFiles[0]);
+   
 
 }
