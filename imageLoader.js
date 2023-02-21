d6cada0f847649cf56bdfddfadd4b62a3baa16ab
ThermiteFe8 <86014765+ThermiteFe8@users.noreply.github.com>
2023-02-21 13:14:00 -0500

    trying to fix modal

--- imageLoader.js
+++ imageLoader.js
@@ -63,3 +63,20 @@ function enlargeImage(imageSource) {
     modal.style.display = "block";
     modalImage.src = imageSource;
 }
+document.addEventListener(
+    "click",
+    function (event) {
+        // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
+        if (
+            event.target.matches(".button-close-modal") ||
+            !event.target.closest(".modal")
+        ) {
+            closeModal()
+        }
+    },
+    false
+)
+
+function closeModal() {
+    document.querySelector(".modal").style.display = "none"
+}
