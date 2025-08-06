547255ba8834167aef11605f18bb0f2f4b885338
thermite <thermitefe8@gmail.com>
2025-08-06 12:32:10 -0400

    blogloader update

---
+++ toggleFont.js
@@ -0,0 +1,25 @@
+var fontArray = ["Times", "OpenDyslexic", "Arial"];
+var fontIndex = 0;
+var currentSize = 3;
+
+
+function toggleFont() {
+	fontIndex++;
+	if(fontIndex > fontArray.length - 1)
+	{
+		fontIndex = 0;
+	}
+  document.getElementById("blogMarkdown").style.fontFamily = fontArray[fontIndex];
+} 
+
+function increaseSize()
+{
+	currentSize++;
+	document.getElementById("blogMarkdown").style.fontSize = currentSize.toString() +'vh';
+}
+
+function decreaseSize()
+{
+	currentSize--;
+	document.getElementById("blogMarkdown").style.fontSize = currentSize.toString() +'vh';
+}
